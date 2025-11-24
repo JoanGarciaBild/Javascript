bb576d84f4ddec0a860a359cb8a3217be97f28a4
JoanGarciaBild <joan.bild@bild.com.mx>
2025-11-24 14:40:46 -0700

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,21 @@
 let js = 'amazing';
-if(js === 'amazing') alert('Javascript is FUN!')
-
 console.log(40+8+23-10)
+
+console.log('Joan');
+
+let firstName = "Alexander";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_mamtilda = "JM";
+let $function = 27;
+
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = 'programmer';
+let job2 = 'teacher';
