3164a9ae8bc9c58637049a71081d732b0233c0fc
JoanGarciaBild <joan.bild@bild.com.mx>
2025-11-24 14:55:28 -0700

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,21 +1,46 @@
-let js = 'amazing';
-console.log(40+8+23-10)
+// let js = 'amazing';
+// console.log(40+8+23-10)
 
-console.log('Joan');
+// console.log('Joan');
 
-let firstName = "Alexander";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Alexander";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let jonas_mamtilda = "JM";
-let $function = 27;
+// //Variable name conventions
+// let jonas_mamtilda = "JM";
+// let $function = 27;
 
-let person = "Jonas";
-let PI = 3.1415;
+// let person = "Jonas";
+// let PI = 3.1415;
 
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
 
-let job1 = 'programmer';
-let job2 = 'teacher';
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+// console.log(myFirstJob)
+
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
